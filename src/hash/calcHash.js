1f77c8c932c5be887082130343521b77bf266a9e
Iuliia Mazaeva <julie_meme@mail.ru>
2023-06-18 19:05:25 +0400

    feat: add solution to calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import * as fs from "fs";
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = './files/fileToCalculateHashFor.txt';
+    const fileData = fs.readFileSync(filePath);
+
+    const hashSum = createHash('sha256');
+    hashSum.update(fileData);
+    const fileHex = hashSum.digest('hex');
+
+    console.log(fileHex);
 };
 
 await calculateHash();
