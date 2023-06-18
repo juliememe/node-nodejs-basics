80fe50cae9f4b15603d68c70f5907c32c9535e32
Iuliia Mazaeva <julie_meme@mail.ru>
2023-06-18 22:47:26 +0400

    feat: add solution to compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = './files/fileToCompress.txt';
+    const archiveFilePath ='./files/archive.gz';
+        const sourceFile = fs.createReadStream(filePath)
+        sourceFile
+            .pipe(createGzip())
+            .pipe(fs.createWriteStream(archiveFilePath));
 };
 
 await compress();
