6b0c2cdb65db4e4a5db371b6fe54481762faad6d
Iuliia Mazaeva <julie_meme@mail.ru>
2023-06-18 13:32:15 +0400

    feat: add solution to list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { readdir } from 'node:fs/promises';
+import { existsSync } from "node:fs";
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    const dirPath = './files';
+
+    if(existsSync(dirPath)) {
+        const files = await readdir(dirPath);
+        for (const fileNames of files)
+            console.log(fileNames);
+    } else{
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
